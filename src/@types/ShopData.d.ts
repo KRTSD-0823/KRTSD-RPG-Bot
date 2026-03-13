b22b438adbbea95122dd5820070978a0daa35d1b
KRTSD0823 <newhiroki1110@outlook.jp>
2026-03-13 12:31:44 +0900

    機能の追加に伴うコードの変更

--- src/@types/ShopData.d.ts
+++ src/@types/ShopData.d.ts
@@ -1,9 +1,6 @@
 // 店に関するデータの型をまとめたファイル
 
-// ショップのデーターのキーの型
-type ShopKeys = "種類" | "名前" | "値段" | "説明" | "威力";
-
-// shop
+// ショップ本体のデータの型
 interface ShopData {
-  data: Array<Record<ShopKeys, string | number>>;
+  data: Array<Items>;
 }
